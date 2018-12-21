609d45d7383a57c51a4835d46f39ddff72672dd0
asura <theworldtocollapse@mail.ru>
2018-12-21 23:19:44 +0500

    template 8 day

---
+++ Puzzles/Puzzles/Tasks/DayEightTaskOne.cs
@@ -0,0 +1,10 @@
+﻿namespace Puzzles.Tasks
+{
+    public class DayEightTaskOne : ITask 
+    {
+        public string Solve(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Puzzles/Puzzles/Tasks/DayEightTaskTwo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Puzzles.Tasks
+{
+    public class DayEightTaskTwo : ITask
+    {
+        public string Solve(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
